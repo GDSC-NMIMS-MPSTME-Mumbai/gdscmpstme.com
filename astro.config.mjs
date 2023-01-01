74f3d11f5b012231ec2fca43b5213d7cf0467324
Rushabh Javeri <javeri.rushabh45@gmail.com>
2023-01-01 21:00:02 +0530

    removed 'site' and 'base' from astro config, changed nav link hover bg color

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,4 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: 'https://GDSC-NMIMS-MPSTME-Mumbai.github.io',
-  base: '/gdscmpstme.com',
 });
