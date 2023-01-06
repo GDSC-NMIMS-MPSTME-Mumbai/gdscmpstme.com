6a594f2424ab16d8f29e8323ab8ddb34d651f44e
Kriti Thakrar <66572570+KritiThakrar@users.noreply.github.com>
2023-01-06 16:22:47 +0530

    Changed colour object

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,16 +9,18 @@ module.exports = {
                 sans: ['Google Sans', ...defaultTheme.fontFamily.sans],
             },
             colors: {
-                'gdsc-grey-light': '#F4F4F4',
-                'gdsc-purple-light': '#E9D1FE',
-                'gdsc-green': '#3CAB5A',
-                'gdsc-green-light': '#E8F5E9',
-                'gdsc-yellow': '#FBBF0E',
-                'gdsc-yellow-light': '#FFF8E1',
-                'gdsc-red': '#EB4A3D',
-                'gdsc-red-light': '#FFEBEE',
-                'gdsc-blue-light': '#75A5F7',
-                'gdsc-blue-lighter': '#E3F2FD',
+                gdsc: {
+                    'grey-light': '#F4F4F4',
+                    'purple-light': '#E9D1FE',
+                    'green': '#3CAB5A',
+                    'green-light': '#E8F5E9',
+                    'yellow': '#FBBF0E',
+                    'yellow-light': '#FFF8E1',
+                    'red': '#EB4A3D',
+                    'red-light': '#FFEBEE',
+                    'blue-light': '#75A5F7',
+                    'blue-lighter': '#E3F2FD',
+                }
             }
         },
     },
