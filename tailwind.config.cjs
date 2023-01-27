4ed17b3a827cd14bc776e163011ca6892b1a7bb0
Pratham Dedhiya <68095465+prathamdedhiya17@users.noreply.github.com>
2023-01-27 23:15:47 +0530

    added lottiefiles and strapi
    added lottie player, fetched data from strapi and refactored the code. also now using gdsc custom colors and images

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,34 @@ module.exports = {
       fontFamily: {
         sans: ['Google Sans', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        gdsc: {
+            'grey': {
+              light: '#F4F4F4',
+            },
+            'green': {
+                light: '#E8F5E9',
+                DEFAULT: '#3CAB5A',
+            },
+            'yellow': {
+                light: '#FFF8E1',
+                DEFAULT: '#FBBF0E',
+            },
+            'blue': {
+                light: '#75A5F7',
+                lighter: '#E3F2FD',
+                DEFAULT: '#EB4A3D',
+            },
+            'red': {
+                light: '#FFEBEE',
+                DEFAULT: '#EB4A3D',
+            },
+            'purple':{
+                light: '#E9D1FE',
+                DEFAULT: '#6B21A8'
+            }
+        },
+      },
     },
   },
   plugins: [],
